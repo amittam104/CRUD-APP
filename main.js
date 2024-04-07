32edd69067084fb02800aa490efb5371a2b8d370
Amit Tambulkar <amittambulkar104@gmail.com>
2024-04-07 13:33:10 +0530

    Connect appwrite backend database, log fetched data to console

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 import "./style.css";
+import { Client, Databases } from "appwrite";
+
+const client = new Client();
+
+client
+  .setEndpoint(import.meta.env.VITE_APPWRITE_URL)
+  .setProject(import.meta.env.VITE_APPWRITE_ACCOUNT);
+
+const db = new Databases(client);
+
+const getTasks = async function () {
+  const response = await db.listDocuments(
+    import.meta.env.VITE_DATABASE_ID,
+    import.meta.env.VITE_COLLECTION_ID
+  );
+
+  console.log(response);
+};
+
+getTasks();
