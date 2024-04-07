4f1a0e9c9e06a345b55be96ca385da90faa64391
Amit Tambulkar <amittambulkar104@gmail.com>
2024-04-07 15:25:18 +0530

    fetch and render tasks

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ import { Client, Databases } from "appwrite";
 
 const client = new Client();
 
+const tasksList = document.querySelector("#tasks-list");
+
 client
   .setEndpoint(import.meta.env.VITE_APPWRITE_URL)
   .setProject(import.meta.env.VITE_APPWRITE_ACCOUNT);
@@ -16,6 +18,19 @@ const getTasks = async function () {
   );
 
   console.log(response);
+
+  response.documents.forEach((task) => renderTasks(task));
 };
 
 getTasks();
+
+const renderTasks = async function (task) {
+  const renderTasks = `
+  <div class="task-wrapper" id="task-${task.$id}">
+    <p class="complete-${task.completed}">${task.body}</p>
+    <strong class="delete">x</strong>
+  </div>
+  `;
+  // console.log(task);
+  tasksList.insertAdjacentHTML("afterbegin", renderTasks);
+};
