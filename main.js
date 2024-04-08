af0b7f5012af9574f3fec1c6d1e606ce814b5f7e
Amit Tambulkar <amittambulkar104@gmail.com>
2024-04-08 07:33:26 +0530

    feat: add update, delete and add features

--- main.js
+++ main.js
@@ -28,17 +28,62 @@ const renderTasks = async function (task) {
   const renderTasks = `
   <div class="task-wrapper" id="task-${task.$id}">
     <p class="complete-${task.completed}">${task.body}</p>
-    <strong class="delete">x</strong>
+    <strong class="delete" id="delete-${task.$id}">x</strong>
   </div>
   `;
   // console.log(task);
   tasksList.insertAdjacentHTML("afterbegin", renderTasks);
+
+  // Delete Task
+  const deleteBtn = document.getElementById(`delete-${task.$id}`);
+  const wrapper = document.getElementById(`task-${task.$id}`);
+
+  deleteBtn.addEventListener("click", function () {
+    db.deleteDocument(
+      import.meta.env.VITE_DATABASE_ID,
+      import.meta.env.VITE_COLLECTION_ID,
+      task.$id
+    );
+
+    wrapper.remove();
+  });
+
+  wrapper.firstElementChild.addEventListener("click", async function (e) {
+    task.completed = !task.completed;
+
+    e.target.className = `complete-${task.completed}`;
+
+    db.updateDocument(
+      import.meta.env.VITE_DATABASE_ID,
+      import.meta.env.VITE_COLLECTION_ID,
+      task.$id,
+      { completed: task.completed }
+    );
+  });
 };
 
-const createTask = async function () {
+const addTask = async function (e) {
+  e.preventDefault();
+
+  const inputTask = e.target.body.value;
+
+  const formError = `<p class="form-error">Please fill the form before submitting</p>`;
+
+  if (!inputTask) {
+    e.target.insertAdjacentHTML("beforeend", formError);
+    return;
+  }
+
   const response = await db.createDocument(
     import.meta.env.VITE_DATABASE_ID,
     import.meta.env.VITE_COLLECTION_ID,
-    ID.unique()
+    ID.unique(),
+    { body: inputTask }
   );
+
+  renderTasks(response);
+
+  form.reset();
 };
+
+form.addEventListener("submit", addTask);
